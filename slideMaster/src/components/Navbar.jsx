566c130604887d53ff34cb16fba246fdd7ae59cb
rahul8945 <rg927686@gmail.com>
2024-07-19 11:09:48 +0530

    Home -presentation- saved link

---
+++ slideMaster/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import openimage from "../assets/open.png";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  let links = [
+    {
+      to: "/",
+      name: "Home",
+    },
+    {
+      to: "/create",
+      name: "Create Presentation",
+    },
+    {
+      to: "/saved",
+      name: "Saved",
+    },
+  ];
+  return (
+    <div className="navbar">
+      <img src={openimage} alt="" />
+      <div className="link">
+        {links.map((ele) => (
+          <NavLink key={ele.to} to={ele.to} className="navlink">
+            {ele.name}
+          </NavLink>
+        ))}
+      </div>
+      <div className="sign">
+        <button>Sign up</button>
+        <button>Log In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ slideMaster/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+    <h1>Welcome To the Slide Master Website</h1>
+    <h3>Here, you can create your  Presentation</h3>
+    </div>
+  )
+}
+
+export default HomePage
